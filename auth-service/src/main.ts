2650e1dfb3058f976052cb00aebf0d062da6efd3
RehanAjiNarwindo <rhnajin@gmail.com>
2026-04-15 12:43:37 +0700

    change name http-exception filr

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AllExceptionsFilter } from 'common/filters/ttp-exception.filter';
+import { AllExceptionsFilter } from 'common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
